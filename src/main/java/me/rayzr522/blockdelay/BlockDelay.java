cd6cb2e783ae8c76bbeacbc601c4523bb591df23
Rayzr522 <rayzr522@gmail.com>
2017-06-27 09:23:37 -0400

    Done, that was easy

--- src/main/java/me/rayzr522/blockdelay/BlockDelay.java
+++ src/main/java/me/rayzr522/blockdelay/BlockDelay.java
@@ -1,137 +1,58 @@
 package me.rayzr522.blockdelay;
 
-import me.rayzr522.blockdelay.utils.MessageHandler;
+import org.bukkit.ChatColor;
+import org.bukkit.block.BlockState;
+import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.logging.Level;
+import org.bukkit.scheduler.BukkitRunnable;
 
 /**
  * @author Rayzr
  */
-public class BlockDelay extends JavaPlugin {
-    private static BlockDelay instance;
-    private MessageHandler messages = new MessageHandler();
-
-    public static BlockDelay getInstance() {
-        return instance;
-    }
+public class BlockDelay extends JavaPlugin implements Listener {
+    private boolean enabled = false;
+    // 5 seconds
+    private long delay = 100l;
 
     @Override
     public void onEnable() {
-        instance = this;
-
-        reload();
+        getServer().getPluginManager().registerEvents(this, this);
     }
 
     @Override
-    public void onDisable() {
-        instance = null;
-    }
-
-    /**
-     * (Re)loads all configs from the disk
-     */
-    public void reload() {
-        saveDefaultConfig();
-        reloadConfig();
-
-        messages.load(getConfig("messages.yml"));
-    }
-
-    /**
-     * If the file is not found and there is a default file in the JAR, it saves the default file to the plugin data folder first
-     *
-     * @param path The path to the config file (relative to the plugin data folder)
-     * @return The {@link YamlConfiguration}
-     */
-    public YamlConfiguration getConfig(String path) {
-        if (!getFile(path).exists() && getResource(path) != null) {
-            saveResource(path, true);
-        }
-        return YamlConfiguration.loadConfiguration(getFile(path));
-    }
-
-    /**
-     * Attempts to save a {@link YamlConfiguration} to the disk, and any {@link IOException}s are printed to the console
-     *
-     * @param config The config to save
-     * @param path   The path to save the config file to (relative to the plugin data folder)
-     */
-    public void saveConfig(YamlConfiguration config, String path) {
-        try {
-            config.save(getFile(path));
-        } catch (IOException e) {
-            getLogger().log(Level.SEVERE, "Failed to save config", e);
-        }
-    }
-
-    /**
-     * @param path The path of the file (relative to the plugin data folder)
-     * @return The {@link File}
-     */
-    public File getFile(String path) {
-        return new File(getDataFolder(), path.replace('/', File.separatorChar));
-    }
-
-    /**
-     * Returns a message from the language file
-     *
-     * @param key     The key of the message to translate
-     * @param objects The formatting objects to use
-     * @return The formatted message
-     */
-    public String tr(String key, Object... objects) {
-        return messages.tr(key, objects);
-    }
-
-    /**
-     * Returns a message from the language file without adding the prefix
-     *
-     * @param key     The key of the message to translate
-     * @param objects The formatting objects to use
-     * @return The formatted message
-     */
-    public String trRaw(String key, Object... objects) {
-        return messages.trRaw(key, objects);
-    }
-
-    /**
-     * Checks a target {@link CommandSender} for a given permission (excluding the permission base). Example:
-     * <p>
-     * <pre>
-     *     checkPermission(sender, "command.use", true);
-     * </pre>
-     * <p>
-     * This would check if the player had the permission <code>"{plugin name}.command.use"</code>, and if they didn't, it would send them the no-permission message from the messages config file.
-     *
-     * @param target      The target {@link CommandSender} to check
-     * @param permission  The permission to check, excluding the permission base (which is the plugin name)
-     * @param sendMessage Whether or not to send a no-permission message to the target
-     * @return Whether or not the target has the given permission
-     */
-    public boolean checkPermission(CommandSender target, String permission, boolean sendMessage) {
-        String fullPermission = String.format("%s.%s", getName(), permission);
-
-        if (!target.hasPermission(fullPermission)) {
-            if (sendMessage) {
-                target.sendMessage(tr("no-permission", fullPermission));
-            }
-
-            return false;
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!sender.hasPermission("BlockDelay.toggle")) {
+            sender.sendMessage(ChatColor.RED + "You need the permission 'BlockDelay.toggle' to do that!");
+            return true;
         }
 
+        enabled = !enabled;
+        sender.sendMessage(ChatColor.GOLD + "Block delay is now " + ChatColor.RED + (enabled ? "enabled" : "disabled"));
         return true;
     }
 
-    /**
-     * @return The {@link MessageHandler} instance for this plugin
-     */
-    public MessageHandler getMessages() {
-        return messages;
-    }
+    @EventHandler(priority = EventPriority.LOW)
+    public void onBlockPlace(BlockPlaceEvent e) {
+        if (e.isCancelled()) {
+            return;
+        }
 
+        if (enabled) {
+            e.setCancelled(true);
+            final BlockState state = e.getBlockPlaced().getState();
+            new BukkitRunnable() {
+                @Override
+                public void run() {
+                    if (e.isCancelled()) {
+                        state.update(true);
+                    }
+                }
+            }.runTaskLater(this, delay);
+        }
+    }
 }
